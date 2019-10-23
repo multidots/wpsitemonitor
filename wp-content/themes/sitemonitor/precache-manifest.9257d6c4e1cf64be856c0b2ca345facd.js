6442ff068123ce79b605d4c77f81920875b5db16
mahamadaslam.shekh@multidots <mahamadaslam.shekh@multidots.com>
2019-10-23 16:38:22 +0530

    update changes

---
+++ wp-content/themes/sitemonitor/precache-manifest.9257d6c4e1cf64be856c0b2ca345facd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110ce227922f59b67d15e83ed767ffe4",
+    "url": "/wp-content/themes/sitemonitor/index.php"
+  },
+  {
+    "revision": "831664c9f812aa22fd62",
+    "url": "/wp-content/themes/sitemonitor/static/css/main.chunk.css"
+  },
+  {
+    "revision": "5cba0e0c117f7469da8e",
+    "url": "/wp-content/themes/sitemonitor/static/js/2.chunk.js"
+  },
+  {
+    "revision": "511f16dc29315d276b34",
+    "url": "/wp-content/themes/sitemonitor/static/js/bundle.js"
+  },
+  {
+    "revision": "831664c9f812aa22fd62",
+    "url": "/wp-content/themes/sitemonitor/static/js/main.chunk.js"
+  },
+  {
+    "revision": "3d149670bb6680da2b5795f169367f42",
+    "url": "/wp-content/themes/sitemonitor/static/media/logo.3d149670.png"
+  }
+]);
